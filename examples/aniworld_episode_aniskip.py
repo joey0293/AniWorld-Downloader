ade40b19e41328004b2a5427f5a1c3d4663dc7f8
joey0293 <225192438+joey0293@users.noreply.github.com>
2026-01-28 20:33:30 +0100

    check system wide install first in autodeps

---
+++ examples/aniworld_episode_aniskip.py
@@ -0,0 +1,12 @@
+import os
+
+from aniworld.models import AniworldEpisode
+
+episode = AniworldEpisode(
+    "https://aniworld.to/anime/stream/prison-school/staffel-1/episode-1"
+)
+
+# Enable AniSkip feature
+os.environ["ANIWORLD_USE_ANISKIP"] = "1"
+
+episode.watch()
