549b0bbfa2764ef0065b278867f6f9bb70704929
joey0293 <225192438+joey0293@users.noreply.github.com>
2026-01-28 17:07:31 +0100

    fetch mal_ids using jikan api

--- src/aniworld/config.py
+++ src/aniworld/config.py
@@ -68,16 +68,13 @@ LULUVDO_USER_AGENT = (
 GLOBAL_SESSION = Session(
     resolver=["doh+google://"],
     headers={
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-Dest": "document",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Sec-Fetch-Mode": "navigate",
         "User-Agent": DEFAULT_USER_AGENT,
+        "Accept": "application/json",
         "Accept-Encoding": "gzip, deflate, br",
-        "Referer": "https://aniworld.to/search",
-        "Priority": "u=0, i",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
     },
+    timeout=10,
 )
 
 logger.debug("Config initialized successfully")
